d7b8eb3e9055f84a098e5a109848d8789b4fdc2e
MiladyGomez <miladyg16@hotmail.com>
2026-05-04 15:13:06 -0500

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,14 @@
+import { useSetRecoilState } from 'recoil';
+import { siteLoadedAtom } from '@utils/recoilStates';
 import styles from './Footer.module.scss';
 
 const Footer = () => {
+
+    const setAssetsLoaded = useSetRecoilState(siteLoadedAtom);
+
+    useEffect(() => {
+        setAssetsLoaded(prevState => ({ ...prevState, footer: true }))
+    }, [])
 
     return (
         <footer className={styles.Container}>
